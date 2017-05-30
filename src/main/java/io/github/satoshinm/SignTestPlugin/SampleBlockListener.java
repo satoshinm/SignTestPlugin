85b1967b5655f558c28f4c57c42f5ee470634cbd
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-05-29 19:27:17 -0700

    Show sign data on place and interact

--- src/main/java/io/github/satoshinm/SignTestPlugin/SampleBlockListener.java
+++ src/main/java/io/github/satoshinm/SignTestPlugin/SampleBlockListener.java
@@ -6,22 +6,58 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockCanBuildEvent;
-import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Sign;
 
-/**
- * Sample block listener
- * @author Dinnerbone
- */
 public class SampleBlockListener implements Listener {
     @EventHandler
-    public void onBlockPhysics(BlockPhysicsEvent event) {
+    public void onBlockPlace(BlockPlaceEvent event) {
         Block block = event.getBlock();
 
-        if ((block.getType() == Material.SAND) || (block.getType() == Material.GRAVEL)) {
-            Block above = block.getRelative(BlockFace.UP);
-            if (above.getType() == Material.IRON_BLOCK) {
-                event.setCancelled(true);
+        dumpSign(block);
+    }
+
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Block block = event.getClickedBlock();
+
+        dumpSign(block);
+    }
+
+    private void dumpSign(Block block) {
+        if (block == null) return;
+
+        if (block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST) {
+            System.out.println("\nblock = "+block);
+
+            MaterialData materialData = block.getState().getData();
+
+            System.out.println("materialData = "+materialData);
+            if (materialData instanceof Sign) {
+                Sign sign = (Sign) materialData;
+
+                try {
+                    System.out.println("getAttachedFace = "+sign.getAttachedFace());
+                } catch (Exception ex) {
+                    System.out.println("getAttachedFace exception: " + ex);
+                    ex.printStackTrace();
+                }
+
+                try {
+                    System.out.println("getFacing = "+sign.getFacing());
+                } catch (Exception ex) {
+                    System.out.println("getFacing exception: " + ex);
+                    ex.printStackTrace();
+                }
+
+                System.out.println("isWallSign? " + sign.isWallSign());
+
+                System.out.println("getData (deprecated) = " + sign.getData());
+            } else {
+                System.out.println("material is not Sign: " + materialData + " at " + block);
             }
         }
     }
